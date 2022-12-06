d25e8c9b1b049091bd07086519fef9ee2dd73f1f
thescientist-ai <lao.bryan.david@gmail.com>
2022-12-06 17:59:36 +0100

    [ISSUE #2] Added comments to the test_solver.py file

--- test_solver.py
+++ test_solver.py
@@ -1,7 +1,13 @@
 from solver import Solver
+from pytest import approx
 import numpy as np
 
 def test_calc_f():
+    ''''
+    Unit test for the function calculating the propulsion F
+    '''
+    
+    # Set parameters to some chosen values
     n = 100
     x = 3*np.ones([n, 2])
     u = 2*np.ones([n, 2])
@@ -12,5 +18,7 @@ def test_calc_f():
     a = Solver(n, x, u, types, delta_t)
     solver_solution = a._Solver__calc_f(vd)
     correct_solution = (vd-u)/a._tau
-    assert (abs(correct_solution-solver_solution) < 1e-6).all() == True
+    
+    # Check if the solver solution and the correct_solution are approximately equal
+    assert approx(solver_solution) == correct_solution
     
