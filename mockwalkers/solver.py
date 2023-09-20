26cd7b59e3e3a7bc145c126a842fd353d1817fa1
Matias Simonetto <m.simonetto@caen.it>
2023-09-20 19:18:45 +0000

    fix solver bug

--- mockwalkers/solver.py
+++ mockwalkers/solver.py
@@ -231,18 +231,6 @@ class Solver:
         e[:, 1] = eytop + eybottom
         return e
 
-    def iterate(self):
-        self._x = self._x + self.delta_t * self._u
-
-        vd = self.__calc_vdterm()
-        self._f = self.__calc_f(vd)
-        self._ksum = np.sum(self.__calc_k(vd), axis=1)
-        acceleration = self._f + self._ksum + self.__calc_e()
-
-        self._u = self._u + self._delta_t * acceleration
-
-        self._current_time = self._current_time + self._delta_t
-
     def __calc_k(self, vd: np.ndarray):
         A = self._int_constant
         R = self._int_radius
@@ -261,11 +249,11 @@ class Solver:
             distance_vec_bstack[:, :, 1]
         )
         distance_mag_nstack = np.sqrt(distance_sqr_nstack)
-        distance_sqr_bstack = np.stack(
-            (distance_sqr_nstack, distance_sqr_nstack), axis=2
+        distance_sqr_bstack = np.broadcast_to(
+            distance_sqr_nstack[:, :, np.newaxis], (*distance_sqr_nstack.shape, 2)
         )
-        distance_mag_bstack = np.stack(
-            (distance_mag_nstack, distance_mag_nstack), axis=2
+        distance_mag_bstack = np.broadcast_to(
+            distance_mag_nstack[:, :, np.newaxis], (*distance_mag_nstack.shape, 2)
         )
 
         if self._vel_option == int(0):
@@ -283,14 +271,16 @@ class Solver:
             np.square(vd_vec_bstack[:, :, 0]) + np.square(vd_vec_bstack[:, :, 1])
         )
 
-        product_org = distance_vec_bstack * vd_vec_bstack
+        # The distance points TO the i walker, but we need the vector pointing away
+        # from it, so we multiply by -1
+        product_org = -1 * distance_vec_bstack * vd_vec_bstack
         product_dot = product_org[:, :, 0] + product_org[:, :, 1]
         product_mag = distance_mag_nstack * vd_mag_nstack
         theta_org = np.arccos(product_dot / product_mag)
         theta_con = theta_org < theta_max
         theta = theta_con
 
-        theta_bstack = np.stack((theta, theta), axis=2)
+        theta_bstack = np.broadcast_to(theta[:, :, np.newaxis], (*theta.shape, 2))
 
         k = (
             A
@@ -316,3 +306,15 @@ class Solver:
             )
         ] *= -1
         return vd
+
+    def iterate(self):
+        self._x = self._x + self.delta_t * self._u
+
+        vd = self.__calc_vdterm()
+        self._f = self.__calc_f(vd)
+        self._ksum = np.sum(self.__calc_k(vd), axis=1)
+        acceleration = self._f + self._ksum + self.__calc_e()
+
+        self._u = self._u + self._delta_t * acceleration
+
+        self._current_time = self._current_time + self._delta_t
