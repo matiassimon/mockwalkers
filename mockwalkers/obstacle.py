e90c2075bab0c7fae0a4972d2c0af86e118c5638
Matias Simonetto <m.simonetto@caen.it>
2023-09-28 16:47:40 +0000

    improve typing

--- mockwalkers/obstacle.py
+++ mockwalkers/obstacle.py
@@ -1,19 +1,16 @@
-from abc import ABC, abstractmethod
-import numpy as np
-from .walkers import Walkers
-
-
-class Obstacle(ABC):
-    def __init__(self, imp_constant):
+class Obstacle:
+    def __init__(self, imp_constant: float):
         self._imp_constant = imp_constant
 
     @property
-    def imp_constant(self):
+    def imp_constant(self) -> float:
         return self._imp_constant
 
 
 class RectangleObstacle(Obstacle):
-    def __init__(self, x, y, dx, dy, imp_constant=0.1):
+    def __init__(
+        self, x: float, y: float, dx: float, dy: float, imp_constant: float = 0.1
+    ):
         self.x1 = min(x, x + dx)
         self.x2 = max(x, x + dx)
         self.y1 = min(y, y + dy)
@@ -21,13 +18,13 @@ class RectangleObstacle(Obstacle):
         super().__init__(imp_constant)
 
     @property
-    def xy(self):
+    def xy(self) -> (float, float):
         return (self.x1, self.y1)
 
     @property
-    def width(self):
+    def width(self) -> float:
         return self.x2 - self.x1
 
     @property
-    def height(self):
+    def height(self) -> float:
         return self.y2 - self.y1
