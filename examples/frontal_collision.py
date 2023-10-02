d78a1ab7dbbafa77a3a00c4b902d7d596a274aec
Matias Simonetto <m.simonetto@caen.it>
2023-10-02 10:31:41 +0000

    update frontal collision example

--- examples/frontal_collision.py
+++ examples/frontal_collision.py
@@ -3,40 +3,68 @@ from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
 import numpy as np
 
-walkers = mckw.Walkers(
-    np.array([[1, 1], [4, 1]]), np.array([[0, 0], [0, 0]]), np.array([0x01, 0x02])
-)
-delta_t = 0.001
+# Obstracles
+obstacles = [mckw.RectangleObstacle(-1, 0, 1, 1), mckw.RectangleObstacle(5, 0, 1, 1)]
+
+# Walkers positions
+x = np.array([[1, 0.5], [4, 0.5]])
+# Walkers types
+type_1_flag = 0x01
+type_2_flag = 0x02
+types = np.array([type_1_flag, type_2_flag])
+
+# Desired velocities
+type_1_vd = (1, 0)
+type_2_vd = (-1, 0)
 vd_calcs = [
-    mckw.RectangleVelocityBooster(0, 0, 10, 10, 1, 0, 0x01),
-    mckw.RectangleVelocityBooster(0, 0, 10, 10, -1, 0, 0x02),
+    mckw.RectangleVelocityBooster(0, 0, 5, 1, *type_1_vd, type_1_flag),
+    mckw.RectangleVelocityBooster(0, 0, 5, 1, *type_2_vd, type_2_flag),
 ]
-obstacles = [mckw.RectangleObstacle(-1, 0, 1, 10)]
+
+# Simulation paramters
+delta_t = 0.01
+final_t = 15
+
+# Plot configuration
+repeat = True
 plot_delta_t = 0.1
-final_t = 30
-repeat = False
 
 s = None
 g = None
 
 fig, ax = plt.subplots()
-ax.set_xlim(0, 5)
-ax.set_ylim(-1, 3)
+ax.set_xlim(-1, 6)
+ax.set_ylim(0, 1)
+ax.set_aspect("equal")
+
+
+def create_solver():
+    return mckw.Solver(
+        mckw.Walkers(x, np.zeros((2, 2)), types),
+        delta_t,
+        vd_calcs,
+        obstacles,
+    )
+
+
+def create_graphic():
+    return mckw.Graphic(ax, s, f_arrows=True, ksum_arrows=True, e_arrows=True)
 
 
 def init_fun():
     global s
     global g
-    s = mckw.Solver(walkers, delta_t, vd_calcs, obstacles)
+    s = create_solver()
     if g is not None:
         g.remove()
-    g = mckw.Graphic(ax, s)
+    g = create_graphic()
+    return g.update_artists
 
 
 def update(t):
     while s.current_time < t:
         s.iterate()
-    g.update()
+    return g.update()
 
 
 ani = FuncAnimation(
@@ -46,5 +74,8 @@ ani = FuncAnimation(
     frames=np.arange(0, final_t, plot_delta_t),
     interval=(1000 * plot_delta_t),
     repeat=repeat,
+    blit=True,
 )
+
 plt.show()
+# ani.save("corridor_example.gif")
